50618baec128bbf628c9f532d831a733258f4f97
Michael Selzer <Michaelselzer28@gmail.com>
2019-06-19 16:57:36 -0400

    updated GUI w text box for file name

--- GUI.py
+++ GUI.py
@@ -1,18 +1,18 @@
 from tkinter import *
-from MetPGA import met_pga_scrape
+#from MetPGAScrape import met_pga_scrape
 
 # click function
-def button_clicked(textEntry, window):
-    enteredText = textEntry.get()  # collect data from text box entry
-    if enteredText == "":
+def button_clicked(textEntry, file_name_entry, window):
+    URL = textEntry.get()  # collect data from text box entry
+    file_name = file_name_entry.get()
+    if URL == "" or file_name == "":
         textToDisplay = "Must enter text. Try again"
     # create another label
     else:
-
         textToDisplay = 'Email list downloaded ... check some folder'
     # clear label first
     Label(window, text=textToDisplay, bg='black',
-          fg='white', font='none 12 bold').grid(row=2, column=0, sticky=W)
+          fg='white', font='none 12 bold').grid(row=3, column=0, sticky=W)
 
 def UI():
     # define window
@@ -26,19 +26,28 @@ def UI():
     Label(window, image=photo, bg='Black').grid(row=0, column=0, sticky=W)
     Label(window, image=photo2, bg='Black').grid(row=0, column=1, sticky=E)
 
-    # create label
+    # create label for URL entry
     labelText = 'Enter tournament competitors URL'
     Label(window, text=labelText, bg='black',
           fg='white', font='none 12 bold').grid(row=1, column=0, sticky=W)
-
     # create text box entry for URL
-    textEntry = Entry(window, width=50, bg='white')
-    textEntry.grid(row=1, column=1, sticky=W)
+    URL_entry = Entry(window, width=50, bg='white')
+    URL_entry.grid(row=1, column=1, sticky=W)
 
-    # add a submit button
+    # create label and text box for file name
+    labelText2 = 'Enter name of document to output'
+    Label(window, text=labelText2, bg='black',
+          fg='white', font='none 12 bold').grid(row=2, column=0, sticky=W)
+    file_name_entry = Entry(window, width=50, bg='white')
+    file_name_entry.grid(row=2, column=1, sticky=W)
 
-    b = Button(window, text='Submit', width=7, command=lambda: button_clicked(textEntry, window)).grid(
-        row=1, column=1, sticky=E)
+
+    # add a submit button for URL and file name
+
+    Button(window, text='Submit', width=7, command=lambda: button_clicked(URL_entry, file_name_entry, window)).grid(
+        row=2, column=1, sticky=E)
 
     # display window
     window.mainloop()
+
+UI()
