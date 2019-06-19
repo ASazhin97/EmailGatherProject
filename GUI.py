49a36193676e4b8d8288ba36d179de5fa41f84c2
Michael Selzer <Michaelselzer28@gmail.com>
2019-06-19 11:02:36 -0400

    working v1 for GUI, to be added to Pauls part

--- GUI.py
+++ GUI.py
@@ -2,9 +2,18 @@ from tkinter import *
 
 
 # click function
-def click():
+def run_program():
     enteredText = textentry.get()  # collect data from text box entry
-    # print(enteredText)
+    if enteredText == "":
+        textToDisplay = "Must enter text. Try again"
+    # create another label
+    else:
+        textToDisplay = 'Email list downloaded ... check some folder'
+    # clear label first
+    Label(window, text="", bg='black',
+          fg='white', font='none 12 bold').grid(row=2, column=0, sticky=W)
+    Label(window, text=textToDisplay, bg='black',
+          fg='white', font='none 12 bold').grid(row=2, column=0, sticky=W)
 
 
 # define window
@@ -12,30 +21,26 @@ window = Tk()
 window.title('Tournament Email Retrieve')
 window.configure(background='black')
 
-# add photos
+# add logo photos
 photo = PhotoImage(file='Logo.gif')
 photo2 = PhotoImage(file='JGHLogo.gif')
 Label(window, image=photo, bg='Black').grid(row=0, column=0, sticky=W)
-Label(window, image=photo2, bg='Black', width=330).grid(row=0, column=1, sticky=W)
+Label(window, image=photo2, bg='Black').grid(row=0, column=1, sticky=E)
 
 # create label
-Label(window, text='Enter tour name', bg='gray',
+Label(window, text='Enter tournament competitors URL', bg='black',
       fg='white', font='none 12 bold').grid(row=1, column=0, sticky=W)
 
-# create text box entry
-textentry = Entry(window, width=20, bg='white')
+# create text box entry for URL
+textentry = Entry(window, width=50, bg='white')
 textentry.grid(row=1, column=1, sticky=W)
 
-# add a button
-Button(window, text='Submit', width=7, command=click).grid(row=1, column=2, sticky=W)
+# add a submit button
+Button(window, text='Submit', width=7, command=run_program).grid(row=1, column=1, sticky=E)
 
-# create another label
-Label(window, text='Enter tournament URL', bg='black',
-      fg='white', font='none 12 bold').grid(row=2, column=0, sticky=W)
-
-#  create a text box
-output = Text(window, width=75, height=6, wrap=WORD, background='White')
-output.grid(row=5, column=0, columnspan=2, sticky=W)
+# #  create a text box
+# output = Text(window, width=55, height=6, wrap=WORD, background='White')
+# output.grid(row=5, column=0, columnspan=2, sticky=W)
 
 # display window
 window.mainloop()
